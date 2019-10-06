ebab1dcc630cdd66b77df841162b7f578eca436e
77uzi77 <55200365+77uzi77@users.noreply.github.com>
2019-10-06 09:48:48 +0800

    二轮第二次上传

--- kv_client.py
+++ kv_client.py
@@ -1,23 +1,28 @@
-import socket
-HOST='localhost'
-PORT=5678
+import socket,argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--host',default='localhost')
+parser.add_argument('--port',type=int,default=5678)
+args = parser.parse_args()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
-s.connect((HOST,PORT))
+s.connect((args.host,args.port))
 # 接收欢迎消息:
 print(s.recv(1024).decode('utf-8'))
 while(True):
-    data = input()
-    if not data or data == 'exit':
-        break
-    key = input()
-    if data == 'SET':
-        value = input()
-        s.send(value.encode('utf-8'))
-    s.send(data.encode('utf-8'))
-    s.send(key.encode('utf-8'))
-    print(s.recv(1024).decode('utf-8'))
-    
-
+    try:
+        data = input()
+        if 'SET' in data:
+            s.send(bytes(data,encoding=('utf-8')))
+        elif 'GET' in data:
+            s.send(bytes(data,encoding=('utf-8')))
+            print(s.recv(1024).decode())
+        elif 'END' in data:
+            print('结束连接')
+            break
+        else :
+            print('请输入正确的命令：SET/GET')
+            print('如想断开连接，请输入：END')
+    except ConnectionError:
+        print("连接服务器失败...请重新连接")
 s.send(b'exit')
 s.close()
--- kv_server.py
+++ kv_server.py
@@ -1,36 +1,37 @@
-import socket
-import threading
-HOST='localhost'
-PORT=5678
-a = {}
+import socket,threading,argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--host',default='localhost')
+parser.add_argument('--port',type=int,default=5678)
+args = parser.parse_args()
+dit = {}
 key = ''
 value = ''
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 监听端口:
-s.bind((HOST,PORT))
+s.bind((args.host,args.port))
 s.listen(5)
-print('Waiting for connection...')
+print('等待连接中...')
 def tcplink(sock, addr):
-    global key,value,a
-    print('Accept new connection from %s:%s...' % addr)
+    print('正在连接 %s:%s...' % addr)
     sock.send(b'Welcome!')
     while True:
         data = sock.recv(1024)
         c = str(data)
         d = c.strip("b")
         e = d.strip("'")
-        f = e.strip("")
-        b = f.split(" ")
-
+        b = e.split(" ")
+        print(b)
         key = b[1]
         if b[0] == 'SET':
             value = b[2]
-            a[key] = value
+            dit[key] = value
+
         elif b[0] == 'GET':
-            if key in a:
-                sock.send(a[key].encode())
+            if  key in dit:
+                sock.send(dit[key].encode())
             else :
-                sock.send('\n'.encode('utf-8'))
+                sock.send('\n'.encode())
         else :
             sock.send('请输入正确的命令：SET/GET'.encode())
     sock.close()
